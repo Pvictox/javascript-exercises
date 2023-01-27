16cacf0cdebe30c2afada8d7d32f517ad8f0ab5f
Pedro <paf.victor@gmail.com>
2023-01-27 19:15:59 -0300

    DONE

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
 const removeFromArray = function() {
+    array = arguments[0] //Array que será removido valores;
+    newArray = [];
+    if (array.length === arguments.length-1){
+        return [];
+    }
+    for (let i=1; i<arguments.length; i++){
+        if (i == 1) newArray = array.filter(elemento => elemento!==arguments[i]);
+        else newArray = newArray.filter(elemento => elemento!==arguments[i]);
+    }
+    return newArray;
+
+
 
 };
 
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(integer1, integer2) {
+    // 1, 4 === 1+2+3+4
+    let sum = 0;
 
+    if (typeof(integer1) === typeof(integer2) && typeof(integer1) === 'number' && integer1 >-1 && integer2>-1){
+        if (integer1 < integer2){
+            for (let i =integer1; i<integer2+1; i++){
+                sum+=i;
+            }
+        }else{
+            for (let i =integer1; i>integer2-1; i--){
+                sum+=i;
+            }
+        }
+    }else{
+        return "ERROR";
+    }
+     
+    
+    return sum;
 };
 
 // Do not edit below this line
