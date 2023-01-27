137127d37328f2f341261054635629b1358f8479
Pedro <paf.victor@gmail.com>
2023-01-27 11:21:55 -0300

    DONE

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(frase, times) {
+    let text = "";
+    if (times < 0){
+        return "ERROR";
+    }
+    for(let i =0; i<times; i++){
+        text+=frase;
+    }
+    return text;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(frase) {
+    let lenFrase = frase.length;
+    let newFrase = "";
+    for (let i =lenFrase-1; i>-1; i--){
+        newFrase+= frase[i];
+    }
+    return newFrase;
 };
 
 // Do not edit below this line
