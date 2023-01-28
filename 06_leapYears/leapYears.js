9d61956b1064317f6fb54ed520904dfeeeab163d
Pedro <paf.victor@gmail.com>
2023-01-28 10:10:31 -0300

    DONE

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
 const leapYears = function() {
-
+    year = arguments[0];
+    if (year%4==0 && year%100!=0){
+        return true;
+    }else if (year%100==0 && year%400==0){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
 const convertToCelsius = function() {
+  let faren = arguments[0];
+  celsius = ((faren-32)*5)/9;
+  return Math.round(celsius*10)/10;
 };
 
 const convertToFahrenheit = function() {
+   let celsius = arguments[0];
+   faren = (celsius*1.8)+32;
+   return Math.round(faren*10)/10;
 };
 
 // Do not edit below this line
