2f93d1069064cfb33ec3fc10097389fd533bcf62
Pedro <paf.victor@gmail.com>
2023-02-16 09:57:38 -0300

    DONE

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,59 @@
 const add = function() {
-	
+	let numbers = [...arguments];
+  const totalAdd = numbers.reduce((add, number) =>{
+    return add + number;
+  })
+  return totalAdd;
 };
 
 const subtract = function() {
-	
+	let numbers = [...arguments];
+  const totalSub = numbers.reduce((sub, num) => {
+      return sub-num;
+  })
+  return totalSub;
 };
 
 const sum = function() {
-	
+	 let args = [...arguments]; //[7,2] [3,5]
+   let totalSum = args.reduce((sum, number) => {
+      let cummulativeSum = number.reduce((newSum, numberArray) =>{
+        return newSum+numberArray;
+      }, 0);
+      return sum+cummulativeSum;
+   },0);
+
+   return totalSum;
 };
 
 const multiply = function() {
-
+    let args = [...arguments];
+    let multResult = args.reduce((result, number) => {
+        let arrayMult = number.reduce((mult, number) => {
+          return mult*number;
+        })
+        return result+arrayMult;
+    }, 0);
+    return multResult
 };
 
 const power = function() {
-	
+  let args = [...arguments];
+  let x = args[0];
+  let result=1;
+  for (let i=0; i<3; i++){
+    result = result*x;
+  }
+  return result;
 };
 
 const factorial = function() {
-	
+  let args = [...arguments];
+  let result = 1;
+  for (let i = args[0]; i>0; i--){
+    result *=i;
+  }
+  return result;
 };
 
 // Do not edit below this line
