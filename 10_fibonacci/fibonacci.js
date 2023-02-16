22448884757658777a7555ea83f8abfae2d18d1e
Pedro <paf.victor@gmail.com>
2023-02-16 11:04:13 -0300

    DONE

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let t1 = 1;
+    let t2 = 1;
+    if (+number < 0){
+        return "OOPS";
+    }
+    if (+number == 1 || +number == 2){
+        return t1;
+    }else{
+        let result;
+        for (let i=2; i<+number; i++){
+            result = t1+t2;
+            t2 = t1
+            t1 = result
+        }
+        return result
+    } 
 };
 
 // Do not edit below this line
